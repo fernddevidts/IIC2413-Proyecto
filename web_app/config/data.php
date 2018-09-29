602b283f7261862f2ece68a4e1ca96b3c61146dd
Fernanda Delgado <fernanda@example.com>
2018-09-29 17:12:17 -0300

    config

---
+++ web_app/config/data.php
@@ -0,0 +1,6 @@
+<?php
+
+$DBgrupo = 'db_grupo11';
+$DBuser = 'grupo11';
+$DBpassword = '124Arboles';
+?>
---
+++ web_app/partials/nav.js
@@ -0,0 +1,5 @@
+
+$(function selected() {
+	$('#myTab a[href^="' + location.pathname.split("/")[3] + '"]').addClass('nav-link active');
+});
+
